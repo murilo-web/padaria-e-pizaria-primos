181469626f30144de03b11239400d6ccccd1c0d5
Murilo Carvalho da Silva <157221991+murilo-web@users.noreply.github.com>
2024-06-11 14:04:29 -0300

    quase acbando

--- public/js/system.js
+++ public/js/system.js
@@ -5,6 +5,87 @@ $(function(){
             
     });
     
+    const slider = document.querySelectorAll('.slider');
+    const btnPrev = document.getElementById('voltar');
+    const btnNExt = document.getElementById('proxima');
+
+    let currentSlide =0;
+
+    function hidenSlide(){
+        slider.forEach(item => item.classList.remove('on'));
+    };
+
+    function showSlider(){
+        slider[currentSlide].classList.add('on');
+    };
+
+    $(btnNExt).click(function () {
+        $(function nextSlider() {
+            hidenSlide()
+            if(currentSlide === slider.length -1){
+                currentSlide = 0
+            }else{
+                currentSlide++
+            }
+            showSlider()
+        })
+        console.log('ok2')  
+    });
+
+    $(btnPrev).click(function () { 
+        $(function prevSlide() {
+            hidenSlide()
+            if(currentSlide === 0){
+                currentSlide = slider.length-1
+                console.log('ok')
+            }else{
+                currentSlide--
+            }
+            showSlider()
+        })
+    });
+})
+
+$(function () { 
+    const slider = document.querySelectorAll('.slider_2');
+    const btnPrev = document.getElementById('voltar_2');
+    const btnNExt = document.getElementById('proxima_2');
+
+    let currentSlide =0;
+
+    function hidenSlide(){
+        slider.forEach(item => item.classList.remove('on'));
+    };
+
+    function showSlider(){
+        slider[currentSlide].classList.add('on');
+    };
+
+    $(btnNExt).click(function () {
+        $(function nextSlider() {
+            hidenSlide()
+            if(currentSlide === slider.length -1){
+                currentSlide = 0
+            }else{
+                currentSlide++
+            }
+            showSlider()
+        })
+        console.log('ok2')  
+    });
+
+    $(btnPrev).click(function () { 
+        $(function prevSlide() {
+            hidenSlide()
+            if(currentSlide === 0){
+                currentSlide = slider.length-1
+                console.log('ok')
+            }else{
+                currentSlide--
+            }
+            showSlider()
+        })
+    });
 })
 
 
